bd872c83f422273a7b6bcd9bacecf0471682f4b0
Maksim Iakunin <iakunin@users.noreply.github.com>
2020-04-20 12:18:11 +0300

    #42 - Updating `rider-spring` to `1.13.0` & removing custom cleanup script (#47)

--- src/test/java/dev/iakunin/codexiabot/bot/StarsUpIntegrationTest.java
+++ src/test/java/dev/iakunin/codexiabot/bot/StarsUpIntegrationTest.java
@@ -2,7 +2,6 @@ package dev.iakunin.codexiabot.bot;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.core.api.dataset.ExpectedDataSet;
-import com.github.database.rider.core.api.dataset.SeedStrategy;
 import dev.iakunin.codexiabot.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +18,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/emptyDatabase.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/emptyDatabase.yml")
     public void emptyDatabase() {
@@ -31,8 +29,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/noStarsUpResults_githubRepoWithoutStat.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/noStarsUpResults_githubRepoWithoutStat.yml")
     public void noStarsUpResults_githubRepoWithoutStat() {
@@ -43,8 +40,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/noStarsUpResults_githubRepoWithOneStat.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/noStarsUpResults_githubRepoWithOneStat.yml")
     public void noStarsUpResults_githubRepoWithOneStat() {
@@ -55,8 +51,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/noStarsUpResults_githubRepoWithTwoStats_withTwoEqualStars.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/noStarsUpResults_githubRepoWithTwoStats_withTwoEqualStars.yml")
     public void noStarsUpResults_githubRepoWithTwoStats_withTwoEqualStars() {
@@ -67,8 +62,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/noStarsUpResults_githubRepoWithTwoStats_starsDecrease.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/noStarsUpResults_githubRepoWithTwoStats_starsDecrease.yml")
     public void noStarsUpResults_githubRepoWithTwoStats_starsDecrease() {
@@ -79,8 +73,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/noStarsUpResults_githubRepoWithTwoStats_starsIncreaseLessThan10.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/noStarsUpResults_githubRepoWithTwoStats_starsIncreaseLessThan10.yml")
     public void noStarsUpResults_githubRepoWithTwoStats_starsIncreaseLessThan10() {
@@ -91,8 +84,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/noStarsUpResults_githubRepoWithThreeStats_starsIncreaseLessThan10.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/noStarsUpResults_githubRepoWithThreeStats_starsIncreaseLessThan10.yml")
     public void noStarsUpResults_githubRepoWithThreeStats_starsIncreaseLessThan10() {
@@ -103,8 +95,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/noStarsUpResults_githubRepoWithTwoStats_starsIncreaseMoreThan10ButLessThan5Percents.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/noStarsUpResults_githubRepoWithTwoStats_starsIncreaseMoreThan10ButLessThan5Percents.yml")
     public void noStarsUpResults_githubRepoWithTwoStats_starsIncreaseMoreThan10ButLessThan5Percents() {
@@ -115,8 +106,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/noStarsUpResults_githubRepoWithThreeStats_starsIncreaseMoreThan10ButLessThan5Percents.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/noStarsUpResults_githubRepoWithThreeStats_starsIncreaseMoreThan10ButLessThan5Percents.yml")
     public void noStarsUpResults_githubRepoWithThreeStats_starsIncreaseMoreThan10ButLessThan5Percents() {
@@ -127,8 +117,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/noStarsUpResults_githubRepoWithTwoStats_happyPath.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/noStarsUpResults_githubRepoWithTwoStats_happyPath.yml")
     public void noStarsUpResults_githubRepoWithTwoStats_happyPath() {
@@ -139,8 +128,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/noStarsUpResults_githubRepoWithThreeStats_happyPath.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/noStarsUpResults_githubRepoWithThreeStats_happyPath.yml")
     public void noStarsUpResults_githubRepoWithThreeStats_happyPath() {
@@ -151,8 +139,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/oneStarsUpResult_githubRepoWithThreeStats_happyPath.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/oneStarsUpResult_githubRepoWithThreeStats_happyPath.yml")
     public void oneStarsUpResult_githubRepoWithThreeStats_happyPath() {
@@ -163,8 +150,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/oneStarsUpResult_githubRepoWithFourStats_happyPath.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/oneStarsUpResult_githubRepoWithFourStats_happyPath.yml")
     public void oneStarsUpResult_githubRepoWithFourStats_happyPath() {
@@ -175,8 +161,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/oneStarsUpResult_noNewGithubStats.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/oneStarsUpResult_noNewGithubStats.yml")
     public void oneStarsUpResult_noNewGithubStats() {
@@ -187,8 +172,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/oneStarsUpResult_starsDecrease.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/oneStarsUpResult_starsDecrease.yml")
     public void oneStarsUpResult_starsDecrease() {
@@ -199,8 +183,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/oneStarsUpResult_starsIncreaseLessThan10.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/oneStarsUpResult_starsIncreaseLessThan10.yml")
     public void oneStarsUpResult_starsIncreaseLessThan10() {
@@ -211,8 +194,7 @@ public class StarsUpIntegrationTest extends AbstractIntegrationTest {
     @Transactional
     @DataSet(
         value = "db-rider/bot/stars-up/initial/oneStarsUpResult_starsIncreaseMoreThan10ButLessThan5Percents.yml",
-        executeScriptsBefore = "db-rider/cleanup.sql", executeScriptsAfter = "db-rider/cleanup.sql",
-        strategy = SeedStrategy.INSERT
+        cleanBefore = true, cleanAfter = true
     )
     @ExpectedDataSet("db-rider/bot/stars-up/expected/oneStarsUpResult_starsIncreaseMoreThan10ButLessThan5Percents.yml")
     public void oneStarsUpResult_starsIncreaseMoreThan10ButLessThan5Percents() {
