4c76ae112bc7418581d74651d188aeb1a0eadeae
iakunin <iakunin@users.noreply.github.com>
2020-04-27 00:10:22 +0300

    #92 - Work in progress

--- src/main/java/dev/iakunin/codexiabot/bot/TooSmall.java
+++ src/main/java/dev/iakunin/codexiabot/bot/TooSmall.java
@@ -8,7 +8,6 @@ import dev.iakunin.codexiabot.github.entity.GithubRepoStat;
 import dev.iakunin.codexiabot.github.entity.GithubRepoStat.LinesOfCode;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -50,8 +49,6 @@ public final class TooSmall implements Runnable {
         return false;
     }
 
-
-    @Slf4j
     @AllArgsConstructor(onConstructor_={@Autowired})
     private static class Submitter {
 
