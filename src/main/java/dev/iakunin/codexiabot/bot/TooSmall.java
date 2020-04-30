672be306490fa5e93de789a7899d878628aaac69
iakunin <iakunin@users.noreply.github.com>
2020-04-30 17:00:48 +0300

    #92 - WIP

--- src/main/java/dev/iakunin/codexiabot/bot/TooSmall.java
+++ src/main/java/dev/iakunin/codexiabot/bot/TooSmall.java
@@ -6,13 +6,14 @@ import dev.iakunin.codexiabot.codexia.CodexiaModule;
 import dev.iakunin.codexiabot.codexia.entity.CodexiaMeta;
 import dev.iakunin.codexiabot.codexia.entity.CodexiaReview;
 import dev.iakunin.codexiabot.github.GithubModule;
+import dev.iakunin.codexiabot.github.entity.GithubRepo;
 import dev.iakunin.codexiabot.github.entity.GithubRepoStat;
 import dev.iakunin.codexiabot.github.entity.GithubRepoStat.GithubApi;
 import dev.iakunin.codexiabot.github.entity.GithubRepoStat.LinesOfCode;
+import io.vavr.Tuple2;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.javatuples.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -44,97 +45,40 @@ public final class TooSmall implements Runnable {
                 final var optional = this.repository.findFirstByGithubRepoOrderByIdDesc(repo);
                 return optional.isEmpty() || optional.get().getState() == TooSmallResult.State.RESET;
             })
-            .map(
-                repo -> {
-                    final Optional<GithubApi> githubApi = this.github.findLastGithubApiStat(repo)
-                        .map(stat -> (GithubApi) stat.getStat());
-
-                    final Optional<GithubRepoStat> linesOfCodeStat = this.github.findLastLinesOfCodeStat(repo);
-
-                    final Optional<LinesOfCode> linesOfCode = linesOfCodeStat
-                        .map(stat -> (LinesOfCode) stat.getStat());
-
-                    if (!(linesOfCodeStat.isPresent() && githubApi.isPresent() && linesOfCode.isPresent())) {
-                        return Optional.<Pair<GithubRepoStat, LinesOfCode.Item>>empty();
-                    }
-
-                    final Optional<LinesOfCode.Item> desiredItem = this.findDesiredItem(
-                        new Pair<>(
-                            githubApi.get(),
-                            linesOfCode.get()
-                        )
-                    );
-
-                    if (desiredItem.isEmpty()) {
-                        return Optional.<Pair<GithubRepoStat, LinesOfCode.Item>>empty();
-                    }
-
-                    return flatMap(
-                        new Pair<>(linesOfCodeStat, desiredItem)
-                    );
-
-//                    return Optional.of(
-//                        new Pair<>(
-//                            linesOfCodeStat.get(),
-//                            desiredItem.get()
-//                        )
-//                    );
-                }
+            .map(this::prepare)
+            .forEach(
+                optional -> optional
+                    .filter(pair -> this.shouldSubmit(pair._2()))
+                    .ifPresent(pair -> this.submitter.submit(pair._1(), pair._2()))
             )
-            .forEach(optional ->
-                optional
-                    .filter(pair -> this.shouldSubmit(pair.getValue1()))
-                    .ifPresent(this.submitter::submit)
-            )
-//            .filter(Optional::isPresent)
-//            .map(Optional::get)
-//            .filter(
-//                pair ->
-//                    pair.getValue0().isPresent()
-//                    && pair.getValue1().isPresent()
-//                    && pair.getValue2().isPresent()
-//            )
-//            .map(
-//                pair -> new Pair<>(
-//                    pair.getValue0().get(),
-//                    this.findDesiredItem(
-//                        pair.getValue1().get(),
-//                        pair.getValue2().get()
-//                    )
-//                )
-//            )
-//            .filter(pair -> pair.getValue1().isPresent())
-//            .map(
-//                pair -> new Pair<>(
-//                    pair.getValue0(),
-//                    pair.getValue1().get()
-//                )
-//            )
-//            .filter(pair -> this.shouldSubmit(pair.getValue1()))
-//            .forEach(this.submitter::submit)
         ;
     }
 
-    public static <A, B> Optional<Pair<A, B>> flatMap(Pair<Optional<A>, Optional<B>> pair) {
-        if (pair.getValue0().isEmpty() || pair.getValue1().isEmpty()) {
-            return Optional.empty();
-        }
-
-        return Optional.of(
-            new Pair<>(
-                pair.getValue0().get(),
-                pair.getValue1().get()
-            )
-        );
+    private Optional<Tuple2<GithubRepoStat, LinesOfCode.Item>> prepare(GithubRepo repo) {
+        return
+            new Tuple2<>(
+                this.github.findLastGithubApiStat(repo),
+                this.github.findLastLinesOfCodeStat(repo)
+            ).map(
+                o -> o.map(stat -> (GithubApi) stat.getStat()),
+                o -> o.map(stat -> (LinesOfCode) stat.getStat())
+            ).apply(
+                (fst, snd) -> fst.isPresent() && snd.isPresent()
+                    ? Optional.of(new Tuple2<>(fst.get(), snd.get()))
+                    : Optional.<Tuple2<GithubApi, LinesOfCode>>empty()
+            ).flatMap(
+                o -> o.apply(this::findDesiredItem)
+            ).flatMap(linesOfCodeItem ->
+                this.github.findLastLinesOfCodeStat(repo)
+                    .map(stat -> new Tuple2<>(stat, linesOfCodeItem))
+            );
     }
 
     // @todo #92 Extract it to a separate class & write a unit-test for it
     private Optional<LinesOfCode.Item> findDesiredItem(
-        Pair<GithubApi, LinesOfCode> pair
+        GithubApi githubStat,
+        LinesOfCode linesOfCodeStat
     ) {
-        final GithubApi githubStat = pair.getValue0();
-        final LinesOfCode linesOfCodeStat = pair.getValue1();
-
         final Optional<LinesOfCode.Item> firstAttempt = linesOfCodeStat
             .getItemList()
             .stream()
@@ -183,9 +127,12 @@ public final class TooSmall implements Runnable {
 
         // @todo #92 TooSmall: add test case with transaction rollback
         @Transactional
-        public void submit(Pair<GithubRepoStat, LinesOfCode.Item> pair) {
-            final CodexiaReview review = this.review(pair);
-            this.repository.save(this.result(pair.getValue0()));
+        public void submit(
+            GithubRepoStat stat,
+            LinesOfCode.Item item
+        ) {
+            final CodexiaReview review = this.review(stat, item);
+            this.repository.save(this.result(stat));
             this.codexia.saveReview(review);
             this.codexia.sendMeta(this.meta(review));
         }
@@ -197,22 +144,25 @@ public final class TooSmall implements Runnable {
                 .setState(TooSmallResult.State.SET);
         }
 
-        private CodexiaReview review(Pair<GithubRepoStat, LinesOfCode.Item> pair) {
+        private CodexiaReview review(
+            GithubRepoStat stat,
+            LinesOfCode.Item item
+        ) {
             return new CodexiaReview()
                 .setText(
                     String.format(
                         "The repo is too small (LoC is %d).",
-                        pair.getValue1().getLinesOfCode()
+                        item.getLinesOfCode()
                     )
                 )
                 .setAuthor(Bot.Type.TOO_SMALL.name())
                 .setReason(
                     String.valueOf(
-                        pair.getValue1().getLinesOfCode()
+                        item.getLinesOfCode()
                     )
                 )
                 .setCodexiaProject(
-                    this.codexia.getCodexiaProject(pair.getValue0().getGithubRepo())
+                    this.codexia.getCodexiaProject(stat.getGithubRepo())
                 );
         }
 
