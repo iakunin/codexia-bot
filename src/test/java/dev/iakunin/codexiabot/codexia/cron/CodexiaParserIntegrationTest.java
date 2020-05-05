e38f4e9d729e198ca2c705738837fc55f700b5b0
iakunin <iakunin@users.noreply.github.com>
2020-05-05 18:12:42 +0300

    #57 - CodexiaParserIntegrationTest: first happy path test

--- src/test/java/dev/iakunin/codexiabot/codexia/cron/CodexiaParserIntegrationTest.java
+++ src/test/java/dev/iakunin/codexiabot/codexia/cron/CodexiaParserIntegrationTest.java
@@ -1,8 +1,16 @@
 package dev.iakunin.codexiabot.codexia.cron;
 
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import dev.iakunin.codexiabot.AbstractIntegrationTest;
 import dev.iakunin.codexiabot.util.WireMockServer;
+import org.cactoos.io.ResourceOf;
+import org.cactoos.text.TextOf;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
@@ -15,7 +23,36 @@ public class CodexiaParserIntegrationTest extends AbstractIntegrationTest {
     @Autowired
     private CodexiaParser codexiaParser;
 
-    // @todo #10 Implement CodexiaParserIntegrationTest
+    @Test
+    @DataSet(
+        value = "db-rider/codexia/cron/codexia-parser/initial/happyPathWithoutGithub.yml",
+        cleanBefore = true, cleanAfter = true
+    )
+    @ExpectedDataSet("db-rider/codexia/cron/codexia-parser/expected/happyPathWithoutGithub.yml")
+    public void happyPathWithoutGithub() {
+        WireMockServer.getInstance().stubFor(
+            get(urlEqualTo("/recent.json?page=0"))
+                .willReturn(ok()
+                    .withHeader("Content-Type",  "application/json")
+                    .withBody(
+                        new TextOf(
+                            new ResourceOf(
+                                "wiremock/codexia/cron/codexia-parser/happyPath.json"
+                            )
+                        ).toString()
+                    )
+                )
+        );
+        WireMockServer.getInstance().stubFor(
+            get(urlEqualTo("/recent.json?page=1"))
+                .willReturn(ok()
+                    .withHeader("Content-Type",  "application/json")
+                    .withBody("[]")
+                )
+        );
+
+        codexiaParser.run();
+    }
 
     @AfterEach
     void after() {
