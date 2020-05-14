e4119cb50c8911ff68f8e9276dd5f9538eb05e65
iakunin <iakunin@users.noreply.github.com>
2020-05-14 11:48:24 +0300

    #55 - ResendErroneousReviewsIntegrationTest

--- src/test/java/dev/iakunin/codexiabot/codexia/cron/ResendErroneousReviewsIntegrationTest.java
+++ src/test/java/dev/iakunin/codexiabot/codexia/cron/ResendErroneousReviewsIntegrationTest.java
@@ -1,12 +1,21 @@
 package dev.iakunin.codexiabot.codexia.cron;
 
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.http.RequestMethod;
 import dev.iakunin.codexiabot.AbstractIntegrationTest;
 import dev.iakunin.codexiabot.util.WireMockServer;
+import dev.iakunin.codexiabot.util.wiremock.Request;
+import dev.iakunin.codexiabot.util.wiremock.Response;
+import dev.iakunin.codexiabot.util.wiremock.Stub;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ContextConfiguration;
 
 @ContextConfiguration(initializers = ResendErroneousReviewsIntegrationTest.Initializer.class)
@@ -15,7 +24,93 @@ public class ResendErroneousReviewsIntegrationTest extends AbstractIntegrationTe
     @Autowired
     private ResendErroneousReviews resendErroneousReviews;
 
-    // @todo #10 Implement ResendErroneousReviewsIntegrationTest
+    @Test
+    @DataSet(
+        value = "db-rider/codexia/cron/resend-erroneous/initial/emptyDatabase.yml",
+        cleanBefore = true, cleanAfter = true
+    )
+    @ExpectedDataSet("db-rider/codexia/cron/resend-erroneous/expected/emptyDatabase.yml")
+    public void emptyDatabase() {
+        resendErroneousReviews.run();
+    }
+
+    @Test
+    @DataSet(
+        value = "db-rider/codexia/cron/resend-erroneous/initial/noErroneous.yml",
+        cleanBefore = true, cleanAfter = true
+    )
+    @ExpectedDataSet("db-rider/codexia/cron/resend-erroneous/expected/noErroneous.yml")
+    public void noErroneous() {
+        resendErroneousReviews.run();
+    }
+
+    @Test
+    @DataSet(
+        value = "db-rider/codexia/cron/resend-erroneous/initial/oneErroneous.yml",
+        cleanBefore = true, cleanAfter = true
+    )
+    @ExpectedDataSet("db-rider/codexia/cron/resend-erroneous/expected/oneErroneous.yml")
+    public void oneErroneous() {
+        WireMockServer.stub(
+            new Stub(
+                new Request(RequestMethod.POST, WireMock.urlPathMatching("/p/\\d+/post")),
+                new Response("Review successfully sent")
+            )
+        );
+
+        resendErroneousReviews.run();
+    }
+
+    @Test
+    @DataSet(
+        value = "db-rider/codexia/cron/resend-erroneous/initial/oneSuccessfulOneErroneous.yml",
+        cleanBefore = true, cleanAfter = true
+    )
+    @ExpectedDataSet("db-rider/codexia/cron/resend-erroneous/expected/oneSuccessfulOneErroneous.yml")
+    public void oneSuccessfulOneErroneous() {
+        WireMockServer.stub(
+            new Stub(
+                new Request(RequestMethod.POST, WireMock.urlPathMatching("/p/\\d+/post")),
+                new Response("Review successfully sent")
+            )
+        );
+
+        resendErroneousReviews.run();
+    }
+
+    @Test
+    @DataSet(
+        value = "db-rider/codexia/cron/resend-erroneous/initial/twoErroneous.yml",
+        cleanBefore = true, cleanAfter = true
+    )
+    @ExpectedDataSet("db-rider/codexia/cron/resend-erroneous/expected/twoErroneous.yml")
+    public void twoErroneous() {
+        WireMockServer.stub(
+            new Stub(
+                new Request(RequestMethod.POST, WireMock.urlPathMatching("/p/\\d+/post")),
+                new Response("Review successfully sent")
+            )
+        );
+
+        resendErroneousReviews.run();
+    }
+
+    @Test
+    @DataSet(
+        value = "db-rider/codexia/cron/resend-erroneous/initial/oneErroneous.yml",
+        cleanBefore = true, cleanAfter = true
+    )
+    @ExpectedDataSet("db-rider/codexia/cron/resend-erroneous/expected/reviewSentWith500.yml")
+    public void reviewSentWith500() {
+        WireMockServer.stub(
+            new Stub(
+                new Request(RequestMethod.POST, WireMock.urlPathMatching("/p/\\d+/post")),
+                new Response(HttpStatus.INTERNAL_SERVER_ERROR.value())
+            )
+        );
+
+        resendErroneousReviews.run();
+    }
 
     @AfterEach
     void after() {
