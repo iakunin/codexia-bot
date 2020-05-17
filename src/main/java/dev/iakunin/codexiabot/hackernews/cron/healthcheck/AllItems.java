90411c36757256567ef381d26ede1ddf30a3d232
iakunin <iakunin@users.noreply.github.com>
2020-05-17 19:06:41 +0300

    #199 - Removing stale puzzle

--- src/main/java/dev/iakunin/codexiabot/hackernews/cron/healthcheck/AllItems.java
+++ src/main/java/dev/iakunin/codexiabot/hackernews/cron/healthcheck/AllItems.java
@@ -4,7 +4,6 @@ import dev.iakunin.codexiabot.github.GithubModule;
 import dev.iakunin.codexiabot.github.GithubModule.Source;
 import dev.iakunin.codexiabot.github.entity.GithubRepoSource;
 import dev.iakunin.codexiabot.hackernews.HackernewsModule;
-import java.util.stream.Stream;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,16 +19,14 @@ public class AllItems implements Runnable {
 
     private final HackernewsModule hackernews;
 
-    // @todo #74 AllItems: get rid of this @Transactional spike
-    //  to do so it's necessary to get rid of Stream<> in repository
-    @Transactional // https://stackoverflow.com/a/40593697/3456163
+    @Transactional
     public void run() {
-        try (Stream<GithubRepoSource> sources = this.github.findAllRepoSources(Source.HACKERNEWS)) {
-            this.hackernews.healthCheckItems(
-                sources.parallel()
-                    .map(GithubRepoSource::getExternalId)
-                    .map(Integer::valueOf)
-            );
-        }
+        this.hackernews.healthCheckItems(
+            this.github
+                .findAllRepoSources(Source.HACKERNEWS)
+                .parallel()
+                .map(GithubRepoSource::getExternalId)
+                .map(Integer::valueOf)
+        );
     }
 }
