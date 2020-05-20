b4352bd96f79005d3cfa78ddc462c250797de8be
iakunin <iakunin@users.noreply.github.com>
2020-05-20 16:00:48 +0300

    migrate.sh fix

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -16,8 +16,8 @@ docker run \
     --interactive \
     --rm \
     --net host \
-    --env LIQUIBASE_HOST=127.0.0.1 \
-    --env LIQUIBASE_PORT=54323 \
+    --env LIQUIBASE_HOST="$REMOTE_DB_HOST" \
+    --env LIQUIBASE_PORT=5432 \
     --env LIQUIBASE_DATABASE=codexia-bot \
     --env LIQUIBASE_USERNAME=codexia-bot \
     --env LIQUIBASE_PASSWORD="$REMOTE_DB_PASSWORD" \
