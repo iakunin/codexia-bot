1bf8af31d2851dc5e4af58e20fee80d66028ec29
iakunin <iakunin@users.noreply.github.com>
2020-05-31 17:40:54 +0300

    #5 - checkstyle: fixing warnings in CodexiaProjectRepositoryIntegrationTest

--- src/test/java/dev/iakunin/codexiabot/codexia/repository/CodexiaProjectRepositoryIntegrationTest.java
+++ src/test/java/dev/iakunin/codexiabot/codexia/repository/CodexiaProjectRepositoryIntegrationTest.java
@@ -9,9 +9,7 @@ import dev.iakunin.codexiabot.github.entity.GithubRepoSource;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import javax.persistence.EntityManager;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class CodexiaProjectRepositoryIntegrationTest extends AbstractIntegrationTest {
 
     @Autowired
-    private EntityManager entityManager;
+    private EntityManager manager;
 
     @Autowired
     private CodexiaProjectRepository repository;
@@ -29,109 +27,113 @@ public class CodexiaProjectRepositoryIntegrationTest extends AbstractIntegration
 
     @Test
     @Transactional
-    public void findByExternalId_found() {
+    public void findByExternalIdFound() {
         final CodexiaProject original = this.createCodexiaProject();
-        entityManager.persist(original);
-        entityManager.flush();
+        this.manager.persist(original);
+        this.manager.flush();
 
-        final CodexiaProject found = repository.findByExternalId(original.getExternalId())
+        final CodexiaProject found = this.repository.findByExternalId(original.getExternalId())
             .orElseThrow(RuntimeException::new);
 
-        assertEquals(original.getUuid().toString(), found.getUuid().toString());
-        assertEquals(original.getExternalId(), found.getExternalId());
-        assertEquals(original.getCoordinates(), found.getCoordinates());
-        assertEquals(original.getAuthor(), found.getAuthor());
-        assertEquals(original.getProjectCreatedAt(), found.getProjectCreatedAt());
+        Assertions.assertEquals(original.getUuid().toString(), found.getUuid().toString());
+        Assertions.assertEquals(original.getExternalId(), found.getExternalId());
+        Assertions.assertEquals(original.getCoordinates(), found.getCoordinates());
+        Assertions.assertEquals(original.getAuthor(), found.getAuthor());
+        Assertions.assertEquals(original.getProjectCreatedAt(), found.getProjectCreatedAt());
 
-        entityManager.remove(found);
+        this.manager.remove(found);
     }
 
     @Test
     @Transactional
-    public void findByExternalId_notFound() {
-        final Optional<CodexiaProject> optional = repository.findByExternalId(faker.random().nextInt(Integer.MAX_VALUE));
+    public void findByExternalIdNotFound() {
+        final Optional<CodexiaProject> optional = this.repository.findByExternalId(
+            this.faker.random().nextInt(Integer.MAX_VALUE)
+        );
 
-        assertTrue(optional.isEmpty());
+        Assertions.assertTrue(optional.isEmpty());
     }
 
     @Test
     @Transactional
-    public void existsByExternalId_found() {
+    public void existsByExternalIdFound() {
         final CodexiaProject project = this.createCodexiaProject();
-        entityManager.persist(project);
-        entityManager.flush();
+        this.manager.persist(project);
+        this.manager.flush();
 
-        final boolean exists = repository.existsByExternalId(project.getExternalId());
+        final boolean exists = this.repository.existsByExternalId(project.getExternalId());
 
-        assertTrue(exists);
+        Assertions.assertTrue(exists);
 
-        entityManager.remove(project);
+        this.manager.remove(project);
     }
 
     @Test
     @Transactional
-    public void existsByExternalId_notFound() {
-        final boolean exists = repository.existsByExternalId(faker.random().nextInt(Integer.MAX_VALUE));
+    public void existsByExternalIdNotFound() {
+        final boolean exists = this.repository.existsByExternalId(
+            this.faker.random().nextInt(Integer.MAX_VALUE)
+        );
 
-        assertFalse(exists);
+        Assertions.assertFalse(exists);
     }
 
     @Test
     @Transactional
-    public void findAllWithoutGithubRepo_found() {
+    public void findAllWithoutGithubRepoFound() {
         final CodexiaProject project = this.createCodexiaProject();
-        entityManager.persist(project);
-        entityManager.flush();
+        this.manager.persist(project);
+        this.manager.flush();
 
-        final var allWithoutGithubRepo = repository.findAllActiveWithoutGithubRepo();
+        final var actual = this.repository.findAllActiveWithoutGithubRepo();
 
-        assertEquals(1, allWithoutGithubRepo.count());
+        Assertions.assertEquals(1, actual.count());
 
-        entityManager.remove(project);
+        this.manager.remove(project);
     }
 
     @Test
     @Transactional
-    public void findAllWithoutGithubRepo_noCodexiaProject() {
-        final var allWithoutGithubRepo = repository.findAllActiveWithoutGithubRepo();
+    public void findAllWithoutGithubRepoNoCodexiaProject() {
+        final var actual = this.repository.findAllActiveWithoutGithubRepo();
 
-        assertEquals(0, allWithoutGithubRepo.count());
+        Assertions.assertEquals(0, actual.count());
     }
 
     @Test
     @Transactional
-    public void findAllWithoutGithubRepo_codexiaProjectWithGithubRepo() {
+    public void findAllWithoutGithubRepoCodexiaProjectWithGithubRepo() {
         final CodexiaProject project = this.createCodexiaProject();
-        final GithubRepo githubRepo = new GithubRepo()
-            .setExternalId(String.valueOf(faker.random().nextInt(Integer.MAX_VALUE)))
+        final GithubRepo repo = new GithubRepo()
+            .setExternalId(String.valueOf(this.faker.random().nextInt(Integer.MAX_VALUE)))
             .setFullName(this.getGithubRepoFullName());
-        final GithubRepoSource githubRepoSource = new GithubRepoSource()
+        final GithubRepoSource source = new GithubRepoSource()
             .setExternalId(String.valueOf(project.getExternalId()))
             .setSource(GithubModule.Source.CODEXIA)
-            .setGithubRepo(githubRepo);
-        entityManager.persist(project);
-        entityManager.persist(githubRepo);
-        entityManager.persist(githubRepoSource);
-        entityManager.flush();
+            .setGithubRepo(repo);
+        this.manager.persist(project);
+        this.manager.persist(repo);
+        this.manager.persist(source);
+        this.manager.flush();
 
-        final var allWithoutGithubRepo = repository.findAllActiveWithoutGithubRepo();
+        final var actual = this.repository.findAllActiveWithoutGithubRepo();
 
-        assertEquals(0, allWithoutGithubRepo.count());
+        Assertions.assertEquals(0, actual.count());
 
-        entityManager.remove(project);
-        entityManager.remove(githubRepoSource);
-        entityManager.remove(githubRepo);
+        this.manager.remove(project);
+        this.manager.remove(source);
+        this.manager.remove(repo);
     }
 
     private CodexiaProject createCodexiaProject() {
         return new CodexiaProject()
-            .setExternalId(faker.random().nextInt(Integer.MAX_VALUE))
+            .setExternalId(this.faker.random().nextInt(Integer.MAX_VALUE))
             .setCoordinates(this.getGithubRepoFullName())
-            .setAuthor(faker.name().username())
+            .setAuthor(this.faker.name().username())
             .setProjectCreatedAt(LocalDateTime.now());
     }
 
     private String getGithubRepoFullName() {
-        return faker.regexify("[a-z]{2,10}/[a-z]{2,10}");
+        return this.faker.regexify("[a-z]{2,10}/[a-z]{2,10}");
     }
 }
