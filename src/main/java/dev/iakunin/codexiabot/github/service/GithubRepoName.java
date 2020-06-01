db95342edbf33009191173865f04daffb5e9f540
iakunin <iakunin@users.noreply.github.com>
2020-06-01 11:01:10 +0300

    #5 - checkstyle: fixing GithubRepoName

--- src/main/java/dev/iakunin/codexiabot/github/service/GithubRepoName.java
+++ src/main/java/dev/iakunin/codexiabot/github/service/GithubRepoName.java
@@ -8,28 +8,30 @@ import org.cactoos.Scalar;
 
 public final class GithubRepoName implements Scalar<String> {
 
+    private static final String DIVIDER = "/";
+
     private final URL url;
 
-    public GithubRepoName(URL url) {
+    public GithubRepoName(final URL url) {
         this.url = url;
     }
 
     @Override
     public String value() throws Exception {
         final String path = this.url.getPath();
-        if (path.length() == 0 || "/".equals(path)) {
+        if (path.length() == 0 || DIVIDER.equals(path)) {
             throw new GithubModule.InvalidRepoNameException(
                 "Url with empty path given"
             );
         }
 
-        final String[] split = path.substring(1).split("/");
+        final String[] split = path.substring(1).split(DIVIDER);
         if (split.length < 2) {
             throw new GithubModule.InvalidRepoNameException(
                 String.format("Invalid github repository name: %s", this.url.toString())
             );
         }
 
-        return Arrays.stream(split).limit(2).collect(Collectors.joining("/"));
+        return Arrays.stream(split).limit(2).collect(Collectors.joining(DIVIDER));
     }
 }
