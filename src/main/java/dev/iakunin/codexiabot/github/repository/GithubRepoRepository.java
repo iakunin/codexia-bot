825f613a321b40e049b6dc69677cc1575b77ebef
iakunin <iakunin@users.noreply.github.com>
2020-06-01 11:42:29 +0300

    #5 - checkstyle: fixing GithubRepoRepository

--- src/main/java/dev/iakunin/codexiabot/github/repository/GithubRepoRepository.java
+++ src/main/java/dev/iakunin/codexiabot/github/repository/GithubRepoRepository.java
@@ -12,37 +12,37 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GithubRepoRepository extends JpaRepository<GithubRepo, Long> {
 
-    Optional<GithubRepo> findByExternalId(String externalId);
+    Optional<GithubRepo> findByExternalId(String id);
 
-    Optional<GithubRepo> findByFullName(String fullName);
+    Optional<GithubRepo> findByFullName(String name);
 
     @Query(
-        "select gr from GithubRepo gr " +
-        "left join GithubRepoStat grs " +
-        "on (gr = grs.githubRepo and grs.type = 'LINES_OF_CODE') " +
-        "where grs.id is null"
+        "select gr from GithubRepo gr "
+        + "left join GithubRepoStat grs "
+        + "on (gr = grs.githubRepo and grs.type = 'LINES_OF_CODE') "
+        + "where grs.id is null"
     )
     Stream<GithubRepo> findAllWithoutLinesOfCode();
 
     @Query(
         value =
-            "select distinct gr.* from github_repo gr " +
-            "join github_repo_source grs1 on gr.id = grs1.github_repo_id " +
-            "join github_repo_source grs2 on gr.id = grs2.github_repo_id " +
-            "where grs1.source = 'CODEXIA' " +
-            "and grs2.source = 'HACKERNEWS' " +
-            "and grs1.deleted_at is null " +
-            "and grs2.deleted_at is null",
+            "select distinct gr.* from github_repo gr "
+            + "join github_repo_source grs1 on gr.id = grs1.github_repo_id "
+            + "join github_repo_source grs2 on gr.id = grs2.github_repo_id "
+            + "where grs1.source = 'CODEXIA' "
+            + "and grs2.source = 'HACKERNEWS' "
+            + "and grs1.deleted_at is null "
+            + "and grs2.deleted_at is null",
         nativeQuery = true
     )
     Stream<GithubRepo> findAllInCodexiaAndHackernews();
 
     @Query(
         value =
-            "select distinct gr.* from github_repo gr " +
-            "join github_repo_source grs1 on gr.id = grs1.github_repo_id " +
-            "where grs1.source = 'CODEXIA' " +
-            "and grs1.deleted_at is null",
+            "select distinct gr.* from github_repo gr "
+            + "join github_repo_source grs1 on gr.id = grs1.github_repo_id "
+            + "where grs1.source = 'CODEXIA' "
+            + "and grs1.deleted_at is null",
         nativeQuery = true
     )
     Stream<GithubRepo> findAllInCodexia();
