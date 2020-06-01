d1109b4c3e34f6fbda27dc838362461ac231cfd9
iakunin <iakunin@users.noreply.github.com>
2020-06-01 13:32:39 +0300

    #5 - checkstyle: fixing SendBadges

--- src/main/java/dev/iakunin/codexiabot/codexia/cron/SendBadges.java
+++ src/main/java/dev/iakunin/codexiabot/codexia/cron/SendBadges.java
@@ -8,18 +8,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * @checkstyle DesignForExtension (500 lines)
+ */
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class SendBadges implements Runnable {
 
-    private final CodexiaBadgeRepository badgeRepository;
+    private final CodexiaBadgeRepository repository;
 
     private final BadgeSender sender;
 
     @Transactional
     public void run() {
-        try (var badges = this.badgeRepository.getAll()) {
+        try (var badges = this.repository.getAll()) {
             new FaultTolerant(
                 badges.map(badge -> () -> this.sender.send(badge)),
                 tr -> log.error("Unable to send badge", tr.getCause())
