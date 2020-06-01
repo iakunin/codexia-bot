6cd27e0249c0f417dbedffc39b86a55a5bc1a581
iakunin <iakunin@users.noreply.github.com>
2020-06-01 12:33:17 +0300

    #5 - checkstyle: fixing SessionFingerprint

--- src/main/java/dev/iakunin/codexiabot/common/config/service/SessionFingerprint.java
+++ src/main/java/dev/iakunin/codexiabot/common/config/service/SessionFingerprint.java
@@ -3,7 +3,7 @@ package dev.iakunin.codexiabot.common.config.service;
 public interface SessionFingerprint {
     String get();
 
-    void set(String sessionFingerprint);
+    void set(String fingerprint);
 
     void unset();
 }
