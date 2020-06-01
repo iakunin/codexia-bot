f761ea6243d9d148d4ee7a223b6044202aed35b6
iakunin <iakunin@users.noreply.github.com>
2020-06-01 12:53:31 +0300

    #5 - checkstyle: fixing ReviewSenderImpl

--- src/main/java/dev/iakunin/codexiabot/codexia/service/ReviewSenderImpl.java
+++ src/main/java/dev/iakunin/codexiabot/codexia/service/ReviewSenderImpl.java
@@ -2,91 +2,106 @@ package dev.iakunin.codexiabot.codexia.service;
 
 import dev.iakunin.codexiabot.codexia.entity.CodexiaReview;
 import dev.iakunin.codexiabot.codexia.entity.CodexiaReviewNotification;
+import dev.iakunin.codexiabot.codexia.entity.CodexiaReviewNotification.Status;
 import dev.iakunin.codexiabot.codexia.repository.CodexiaReviewNotificationRepository;
 import dev.iakunin.codexiabot.codexia.sdk.CodexiaClient;
 import feign.FeignException;
 import java.nio.ByteBuffer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.cactoos.scalar.Ternary;
+import org.cactoos.scalar.Unchecked;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * @checkstyle DesignForExtension (500 lines)
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ReviewSenderImpl implements ReviewSender {
 
-    private final CodexiaReviewNotificationRepository codexiaReviewNotificationRepository;
+    private final CodexiaReviewNotificationRepository repository;
 
-    private final CodexiaClient codexiaClient;
+    private final CodexiaClient client;
 
     @Override
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void send(CodexiaReview review) {
-        final CodexiaReviewNotification savedNotification = this.saveNewNotification(review);
+    public void send(final CodexiaReview review) {
+        final CodexiaReviewNotification saved = this.saveNewNotification(review);
         final ResponseEntity<String> response;
         try {
-            response = this.codexiaClient.createReview(
+            response = this.client.createReview(
                 review.getCodexiaProject().getExternalId(),
                 review.getText(),
                 review.getUuid().toString()
             );
-        } catch (FeignException e) {
-            if (e.status() != CodexiaClient.ReviewStatus.ALREADY_EXISTS.httpStatus()) {
+        } catch (final FeignException ex) {
+            if (ex.status() != CodexiaClient.ReviewStatus.ALREADY_EXISTS.httpStatus()) {
                 log.warn(
                     "Exception occurred during review creation in Codexia; externalId='{}'",
                     review.getCodexiaProject().getExternalId(),
-                    e
+                    ex
                 );
             }
-            this.saveExceptionalNotification(savedNotification, e);
+            this.saveExceptionalNotification(saved, ex);
             return;
         }
-        this.saveSuccessfulNotification(savedNotification, response);
+        this.saveSuccessfulNotification(saved, response);
     }
 
-    private CodexiaReviewNotification saveNewNotification(CodexiaReview review) {
-        return this.codexiaReviewNotificationRepository.save(
+    private CodexiaReviewNotification saveNewNotification(final CodexiaReview review) {
+        return this.repository.save(
             new CodexiaReviewNotification()
                 .setCodexiaReview(review)
-                .setStatus(CodexiaReviewNotification.Status.NEW)
+                .setStatus(Status.NEW)
         );
     }
 
     private void saveSuccessfulNotification(
-        CodexiaReviewNotification notification,
-        ResponseEntity<String> response
+        final CodexiaReviewNotification notification,
+        final ResponseEntity<String> response
     ) {
-        this.codexiaReviewNotificationRepository.save(
+        this.repository.save(
             notification
-                .setStatus(CodexiaReviewNotification.Status.SUCCESS)
+                .setStatus(Status.SUCCESS)
                 .setResponseCode(response.getStatusCodeValue())
                 .setResponse(response.toString())
         );
     }
 
     private void saveExceptionalNotification(
-        CodexiaReviewNotification notification,
-        FeignException e
+        final CodexiaReviewNotification notification,
+        final FeignException exception
     ) {
-        this.codexiaReviewNotificationRepository.save(
+        this.repository.save(
             notification
                 .setStatus(
                     // @todo #19 rewrite via custom Feign exceptions
-                    e.status() == CodexiaClient.ReviewStatus.ALREADY_EXISTS.httpStatus()
-                        ? CodexiaReviewNotification.Status.SUCCESS
-                        : CodexiaReviewNotification.Status.ERROR
+                    this.resolveStatus(exception)
                 )
-                .setResponseCode(e.status())
+                .setResponseCode(exception.status())
                 .setResponse(
                     new String(
-                        e.responseBody()
+                        exception.responseBody()
                             .orElse(ByteBuffer.allocate(0))
                             .array()
                     )
                 )
         );
+    }
+
+    private Status resolveStatus(final FeignException exception) {
+        return
+            new Unchecked<>(
+                new Ternary<>(
+                    exception.status() == CodexiaClient.ReviewStatus.ALREADY_EXISTS.httpStatus(),
+                    Status.SUCCESS,
+                    Status.ERROR
+                )
+            ).value();
     }
 }
