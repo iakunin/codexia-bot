ea0e4f3f97975d4de62bf17027a164d22a02d235
iakunin <iakunin@users.noreply.github.com>
2020-06-01 13:17:48 +0300

    #5 - checkstyle: fixing CodexiaReviewNotificationRepository

--- src/main/java/dev/iakunin/codexiabot/codexia/repository/CodexiaReviewNotificationRepository.java
+++ src/main/java/dev/iakunin/codexiabot/codexia/repository/CodexiaReviewNotificationRepository.java
@@ -5,7 +5,8 @@ import dev.iakunin.codexiabot.codexia.entity.CodexiaReviewNotification;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CodexiaReviewNotificationRepository extends JpaRepository<CodexiaReviewNotification, Long> {
+public interface CodexiaReviewNotificationRepository
+    extends JpaRepository<CodexiaReviewNotification, Long> {
 
     List<CodexiaReviewNotification> findAllByCodexiaReviewOrderByIdDesc(CodexiaReview review);
 }
