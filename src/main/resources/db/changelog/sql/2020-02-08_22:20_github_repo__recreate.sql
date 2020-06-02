daa63998c0e52831e9e0c2646732f38225ceff0a
iakunin <iakunin@users.noreply.github.com>
2020-06-02 13:45:50 +0300

    #5 - editorconfig: fixing formatting

--- src/main/resources/db/changelog/sql/2020-02-08_22:20_github_repo__recreate.sql
+++ src/main/resources/db/changelog/sql/2020-02-08_22:20_github_repo__recreate.sql
@@ -1,27 +1,28 @@
 drop table "github_repo";
 
-create table "github_repo" (
-   "id" bigserial primary key,
-   "uuid" uuid not null unique,
-   "external_id" varchar not null unique,
+create table "github_repo"
+(
+    "id"              bigserial primary key,
+    "uuid"            uuid    not null unique,
+    "external_id"     varchar not null unique,
 
-   "full_name" varchar,
-   "html_url" varchar,
-   "description" varchar,
-   "language" varchar,
-   "homepage" varchar,
+    "full_name"       varchar,
+    "html_url"        varchar,
+    "description"     varchar,
+    "language"        varchar,
+    "homepage"        varchar,
 
-   "forks" integer,
-   "watchers" integer,
-   "stars" integer,
-   "releases" integer,
-   "used_by" integer,
+    "forks"           integer,
+    "watchers"        integer,
+    "stars"           integer,
+    "releases"        integer,
+    "used_by"         integer,
 
-   "lines_of_code" json,
+    "lines_of_code"   json,
 
-   "repo_created_at" timestamp,
-   "pushed_at" timestamp,
+    "repo_created_at" timestamp,
+    "pushed_at"       timestamp,
 
-   "created_at" timestamp,
-   "updated_at" timestamp
+    "created_at"      timestamp,
+    "updated_at"      timestamp
 );
